6b6ab45540b124eae65b8f093c0b6eb49e1e99d0
AamirSohail07000 <sohail.aamir07000@gmail.com>
2025-09-11 14:29:20 +0530

    Use base model instead of constructor

--- models.py
+++ models.py
@@ -1,14 +1,17 @@
-class Products:
+from pydantic import BaseModel
+
+class Products(BaseModel):
   id: int
   name: str
   description: str
   price: float
   quantity: int
 
-  def __init__(self, id: int, name: str, description: str, price: float, quantity: int):
-    self.id = id
-    self.name = name
-    self.description = description
-    self.price = price
-    self=quantity = quantity
+  # Used BaseModel instead of this
+  # def __init__(self, id: int, name: str, description: str, price: float, quantity: int):
+  #   self.id = id
+  #   self.name = name
+  #   self.description = description
+  #   self.price = price
+  #   self=quantity = quantity
 
